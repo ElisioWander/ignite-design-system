476c59376f5cb2cce1b5027d1d51c4a4630df9ce
Elisio Wander <elisio@datac.com.br>
2023-02-08 12:38:51 -0300

    Component: Box & Componente: Text

---
+++ packages/docs/src/stories/Text.stories.tsx
@@ -0,0 +1,20 @@
+import { Meta, StoryObj } from '@storybook/react'
+import { Text, TextProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Typography/Text',
+  component: Text,
+  args: {
+    children:
+      'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laborum laboriosam commodi nobis quod! Ad nemo totam excepturi! Accusantium facilis amet id nemo, facere est optio quisquam esse, sunt voluptate alias.',
+  },
+} as Meta<TextProps>
+
+export const Primary: StoryObj<TextProps> = {}
+
+export const CustomTag: StoryObj<TextProps> = {
+  args: {
+    children: 'Strong text',
+    as: 'strong',
+  },
+}
